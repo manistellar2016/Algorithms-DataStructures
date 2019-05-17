de57edb4f4d46fdacda90cdae17c48c307786edb
Mani Parnandi <manikumar.p@gmail.com>
2019-05-16 23:03:21 -0700

    Adding Searching and Sorting Algorithm class files

---
+++ AlgorithmsDatastructures/Algorithms_Datastructures/Algorithms/Searching/SearchingAlgorithms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Datastructures.Algorithms.Searching
+{
+    /// <summary>
+    /// This class library consists of various search algorithms such as:
+    /// Linear Search, Binary Search, Jump Search, Interpolation Search, Exponential Search
+    /// Fibonacci Search, Ubiquotus Binary Search etc.
+    /// </summary>
+    class SearchingAlgorithms
+    {
+
+    }
+}
---
+++ AlgorithmsDatastructures/Algorithms_Datastructures/Algorithms/Sorting/SortAlgorithms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Datastructures.Algorithms.Sorting
+{
+    /// <summary>
+    /// This class library consists of various sort algorithms such as:
+    /// Selection Sort, Insertion Sort, Quick Sort, Bubble Sort, Heap Sort, Merge Sort, Bucket Sort, Radix Sort etc.
+    /// Shell Sort, Pancake Sorting
+    /// </summary>
+    class SortAlgorithms
+    {
+    }
+}
